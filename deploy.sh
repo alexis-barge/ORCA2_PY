53cc251c876642da06fb1673cf3523cf9a4f592a
Alexis Barge <138531178+alexis-barge@users.noreply.github.com>
2026-06-12 14:40:38 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,14 @@ echo "--------------------------"
 if [ ! -d "FORCING" ]; then mkdir FORCING; fi
 
 if [ ! -f "INPUTS/ORCA2_ICE_v5.0.0.tar.gz" ]; then 
-   wget  -O FORCING/ORCA2_ICE_v5.0.0.tar.gz || exit 2
+   wget "https://gws-access.jasmin.ac.uk/public/nemo/sette_inputs/r5.0.0/ORCA2_ICE_v5.0.0.tar.gz" -O FORCING/ORCA2_ICE_v5.0.0.tar.gz || exit 2
 fi
 if [ ! -f "INPUTS/ORCA_R2_zps_domcfg.nc" ]; then tar -xf ORCA2_ICE_v5.0.0.tar.gz .; fi
 echo "   --> DONE DOWNLOAD"
 
 
 #   II. Set up config
-# =====================
+# =====================:
 echo "----------------------------"
 echo "   CREATE CONFIG ORCA2_PY   "
 echo "----------------------------"
@@ -26,7 +26,7 @@ touch ../work_cfgs.txt
 if ! grep -qx "ORCA2_PY OCE ICE" ../work_cfgs.txt; then
     echo "ORCA2_PY OCE ICE" >> ../work_cfgs.txt
 fi
-cp arch-JEANZAY_GCC_CPL.fcm ../../arch/
+cp arch-ORCA2_PY_GCC_CPL.fcm ../../arch/
 echo "   --> DONE CONFIG"
 
 
@@ -36,7 +36,7 @@ echo "------------------"
 echo "   COMPILE NEMO   "
 echo "------------------"
 
-../../makenemo -m ORCA2_GCC -r ORCA2_PY -n ORCA2_PY -j 3 || exit 4
+../../makenemo -m ORCA2_PY_GCC_CPL -r ORCA2_PY -n ORCA2_PY -j 3 || exit 4
 
 # just in case - reinit EXP00
 for file in EXPREF/* job.ksh
